122fb3a78b93bfacdbe582f77275426cdad33f77
Teja Bayya <bayyatej.dev@gmail.com>
2018-06-04 21:34:20 -0400

    implement view recycling

--- 03 - Multiscreen Apps/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ 03 - Multiscreen Apps/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,8 +18,12 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -31,11 +35,78 @@ public class MainActivity extends AppCompatActivity
 
 		// Set the content of the activity to use the activity_main.xml layout file
 		setContentView(R.layout.activity_main);
+
+		// Find the View that shows the numbers category
+		TextView numbers = (TextView) findViewById(R.id.numbers);
+
+		// Set a click listener on that View
+		numbers.setOnClickListener(new View.OnClickListener()
+		{
+			// The code in this method will be executed when the numbers View is clicked on.
+			@Override
+			public void onClick(View view)
+			{
+				//number intent
+				Intent numbersIntent = new Intent(MainActivity.this,
+						NumbersActivity.class);
+				startActivity(numbersIntent);
+			}
+		});
+
+		//OnClick for remaining Views
+		
+		//Get remaining views
+		TextView familyMembers = findViewById(R.id.family);
+		TextView colors = findViewById(R.id.colors);
+		TextView phrases = findViewById(R.id.phrases);
+		
+		//onClick for familyMembers
+		familyMembers.setOnClickListener(new View.OnClickListener()
+		{
+			// The code in this method will be executed when the familyMembers View is clicked on.
+			@Override
+			public void onClick(View view)
+			{
+				//familyMember intent
+				Intent familyMembersIntent = new Intent(MainActivity.this,
+						FamilyActivity.class);
+				startActivity(familyMembersIntent);
+			}
+		});
+		
+		//onClick for Colors
+		colors.setOnClickListener(new View.OnClickListener()
+		{
+			// The code in this method will be executed when the colors View is clicked on.
+			@Override
+			public void onClick(View view)
+			{
+				//color intent
+				Intent colorsIntent = new Intent(MainActivity.this,
+						ColorsActivity.class);
+				startActivity(colorsIntent);
+			}
+		});
+		
+		//onClick for phrases
+		phrases.setOnClickListener(new View.OnClickListener()
+		{
+			// The code in this method will be executed when the phrases View is clicked on.
+			@Override
+			public void onClick(View view)
+			{
+				//phrase intent
+				Intent phrasesIntent = new Intent(MainActivity.this,
+						PhrasesActivity.class);
+				startActivity(phrasesIntent);
+			}
+		});
+		
 	}
 
-	public void openNumbersList(View view)
-	{
-		Intent intent = new Intent(this, NumbersActivity.class);
-		startActivity(intent);
-	}
+//	public void openNumbersList(View view)
+//	{
+//		Intent intent = new Intent(this, NumbersActivity.class);
+//		startActivity(intent);
+//	}
 }

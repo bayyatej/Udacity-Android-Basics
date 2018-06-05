fc4ceaa1583590c42738c36dbbeb8b2dd3325da0
Teja Bayya <bayyatej.dev@gmail.com>
2018-06-04 22:59:54 -0400

    add Word class

---
+++ 03 - Multiscreen Apps/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+/**
+ * {@link Word} Represents a vocabulary word the user wants to learn.
+ * Contains a default translation and a Miwok translation for that word
+ */
+public class Word
+{
+	private String mMiwokTranslation;
+	private String mDefaultTranslation;
+
+	/**
+	 * Constructs a word object using given parameters
+	 *
+	 * @param miwokTranslation:  String translation of word in Miwok
+	 * @param defaultTranslation String translation of word in default language
+	 */
+	public Word(String miwokTranslation, String defaultTranslation)
+	{
+		mMiwokTranslation = "";
+		mMiwokTranslation = miwokTranslation;
+		mDefaultTranslation = defaultTranslation;
+	}
+
+	/**
+	 * Returns Miwok translation of Word
+	 *
+	 * @return Miwok translation of Word
+	 */
+	public String getMiwokTranslation()
+	{
+		return mMiwokTranslation;
+	}
+
+	/**
+	 * Returns DefaultTranslation
+	 *
+	 * @return default translation of Word
+	 */
+	public String getDefaultTranslation()
+	{
+		return mDefaultTranslation;
+	}
+}

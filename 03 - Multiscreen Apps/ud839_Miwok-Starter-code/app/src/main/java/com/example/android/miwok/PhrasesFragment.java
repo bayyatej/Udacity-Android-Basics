81914c7aac332c70a74262032927f816c04d6d83
Teja Bayya <bayyatej.dev@gmail.com>
2018-06-09 11:37:50 -0400

    implement all fragments

--- 03 - Multiscreen Apps/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/PhrasesFragment.java
+++ 03 - Multiscreen Apps/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/PhrasesFragment.java
@@ -1,19 +1,62 @@
 package com.example.android.miwok;
 
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PhrasesFragment extends Fragment
 {
-
+	private MediaPlayer mMediaPlayer;
+	private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener()
+	{
+		@Override
+		public void onCompletion(MediaPlayer mp)
+		{
+			releaseMediaPlayer();
+		}
+	};
+	private AudioManager mAudioManager;
+	private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener()
+	{
+		@Override
+		public void onAudioFocusChange(int focusChange)
+		{
+			if (focusChange == AudioManager.AUDIOFOCUS_GAIN)
+			{
+				if (mMediaPlayer != null)
+				{
+					mMediaPlayer.start();
+				}
+			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS)
+			{
+				if (mMediaPlayer != null)
+				{
+					mMediaPlayer.stop();
+				}
+				releaseMediaPlayer();
+			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
+			{
+				if (mMediaPlayer != null)
+				{
+					mMediaPlayer.pause();
+					mMediaPlayer.seekTo(0);
+				}
+			}
+		}
+	};
 
 	public PhrasesFragment()
 	{
@@ -25,9 +68,68 @@ public class PhrasesFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState)
 	{
-		TextView textView = new TextView(getActivity());
-		textView.setText(R.string.hello_blank_fragment);
-		return textView;
+		View rootView = inflater.inflate(R.layout.word_list, container, false);
+		ListView listView = rootView.findViewById(R.id.list);
+		WordAdapter adapter;
+		mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+		final ArrayList<Word> words = new ArrayList<>();
+		words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
+		words.add(new Word("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
+		words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+		words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
+		words.add(new Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+		words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
+		words.add(new Word("Yes, I'm coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
+		words.add(new Word("I'm coming.", "әәnәm", R.raw.phrase_im_coming));
+		words.add(new Word("Let's go.", "yoowutis", R.raw.phrase_lets_go));
+		words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
+
+		adapter = new WordAdapter(getActivity(), words, R.color.category_numbers);
+
+
+		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			{
+				releaseMediaPlayer();
+				Word word = words.get(position);
+				int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+				if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+				{
+					mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
+					mMediaPlayer.start();
+					mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+				}
+			}
+		});
+
+		return rootView;
 	}
 
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+
+		// When the activity is stopped, release the media player resources because we won't
+		// be playing any more sounds.
+		releaseMediaPlayer();
+	}
+
+	/**
+	 * releases media player if it is not null and abandons audio focus to clear up resources
+	 */
+	public void releaseMediaPlayer()
+	{
+		if (mMediaPlayer != null)
+		{
+			mMediaPlayer.release();
+			mMediaPlayer = null;
+			mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+		}
+	}
 }

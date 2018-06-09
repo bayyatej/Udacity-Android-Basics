fb1fa67081fd6676a64f715bbcbf384872fdce50
Teja Bayya <bayyatej.dev@gmail.com>
2018-06-09 17:29:25 -0400

    implement Destination class

---
+++ 03 - Multiscreen Apps/TourGuide/app/src/main/java/com/example/android/tourguide/Destination.java
@@ -0,0 +1,53 @@
+package com.example.android.tourguide;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class Destination
+{
+	private String mName;
+	private String mPhoneNumber;
+	/**
+	 * Query string for google maps url api
+	 */
+	private String mApiQueryUrl;
+	private int mImageId;
+	private static final String API_URL = "https://www.google.com/maps/dir/?api=1";
+	private static final String API_LOCATION = "%2C+Novi%2C+MI";
+	private static final String BAD_API_URL = "BAD API URL";
+
+	public Destination(String name, String phoneNumber, int imageId)
+	{
+		mName = name;
+		mPhoneNumber = phoneNumber;
+		mImageId = imageId;
+
+		try
+		{
+			mApiQueryUrl = API_URL + URLEncoder.encode(mName, "utf-8") + API_LOCATION;
+		} catch (UnsupportedEncodingException e)
+		{
+			mApiQueryUrl = BAD_API_URL;
+		}
+	}
+
+	public String getName()
+	{
+		return mName;
+	}
+
+	public String getPhoneNumber()
+	{
+		return mPhoneNumber;
+	}
+
+	public String getApiQueryUrl()
+	{
+		return mApiQueryUrl;
+	}
+
+	public int getImageId()
+	{
+		return mImageId;
+	}
+}

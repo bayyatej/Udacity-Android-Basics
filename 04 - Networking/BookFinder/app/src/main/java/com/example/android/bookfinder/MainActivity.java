4f67d9a223dca734efe5f83da30ade8e15ce5302
Teja Bayya <bayyatej.dev@gmail.com>
2018-07-07 13:09:10 -0400

    create MainActivity

--- 04 - Networking/BookFinder/app/src/main/java/com/example/android/bookfinder/MainActivity.java
+++ 04 - Networking/BookFinder/app/src/main/java/com/example/android/bookfinder/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.android.bookfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BookActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity
 {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.book_list);
+		setContentView(R.layout.activity_main);
 	}
 }

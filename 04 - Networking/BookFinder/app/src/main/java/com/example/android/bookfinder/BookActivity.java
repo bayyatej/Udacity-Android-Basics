a95e7cdfc8a0f37a79595e83087ea9cfe3304987
Teja Bayya <bayyatej.dev@gmail.com>
2018-07-07 12:59:36 -0400

    add BookAdapter and BookActivity

--- 04 - Networking/BookFinder/app/src/main/java/com/example/android/bookfinder/BookActivity.java
+++ 04 - Networking/BookFinder/app/src/main/java/com/example/android/bookfinder/BookActivity.java
@@ -1,15 +1,14 @@
 package com.example.android.bookfinder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class BookList extends AppCompatActivity
+public class BookActivity extends AppCompatActivity
 {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_book_list);
 	}
 }
---
+++ 04 - Networking/BookFinder/app/src/main/java/com/example/android/bookfinder/BookAdapter.java
@@ -0,0 +1,49 @@
+package com.example.android.bookfinder;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookAdapter extends ArrayAdapter
+{
+	public BookAdapter(@NonNull Context context, int resource, @NonNull List<Book> books)
+	{
+		super(context, 0, books);
+	}
+
+	@NonNull
+	@Override
+	public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+	{
+		View bookListView = convertView;
+		final Book book = (Book) getItem(position);
+		String title = book.getmTitle();
+		ArrayList<String> authors = (ArrayList<String>) book.getmAuthors();
+		String authorsString = "";
+
+		for (String author : authors)
+		{
+			authorsString += author + ", ";
+		}
+		authorsString = authorsString.substring(0, authorsString.length() - 2);
+
+		((TextView) bookListView.findViewById(R.id.book_title)).setText(title);
+		((TextView) bookListView.findViewById(R.id.book_author)).setText(authorsString);
+
+		if (bookListView != null)
+		{
+			bookListView = LayoutInflater.from(getContext()).inflate(R.layout.activity_book_list, parent, false);
+		}
+
+
+		return bookListView;
+	}
+}

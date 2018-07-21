995c4efc5539c55a0ee290eb0b6244a0cedb9167
Teja Bayya <bayyatej.dev@gmail.com>
2018-07-21 13:49:01 -0400

    add News, NewsActivity, and QueryUtils classes

--- 04 - Networking/NewsApp/app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ 04 - Networking/NewsApp/app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.android.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+	private Intent newsListIntent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		newsListIntent = new Intent(getApplicationContext(), NewsActivity.class);
+	}
+
+	public void onClickSearch(View view)
+	{
+		String query = ((EditText) findViewById(R.id.search)).getText().toString();
+		newsListIntent.putExtra("query", query);
+		startActivity(newsListIntent);
 	}
 }

08335715f6aa0adb1b0d2d96cd74559fc042c85c
Teja Bayya <bayyatej.dev@gmail.com>
2018-08-01 21:40:00 -0400

    implement Delete

--- 05 - Data Storage/Pets/app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ 05 - Data Storage/Pets/app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -211,7 +211,23 @@ public class PetProvider extends ContentProvider
 	@Override
 	public int delete(@NonNull Uri uri, String selection, String[] selectionArgs)
 	{
-		return 0;
+		// Get writeable database
+		SQLiteDatabase database = mPetDBHelper.getWritableDatabase();
+
+		final int match = sUriMatcher.match(uri);
+		switch (match)
+		{
+			case PETS:
+				// Delete all rows that match the selection and selection args
+				return database.delete(PetsEntry.TABLE_NAME, selection, selectionArgs);
+			case PET_ID:
+				// Delete a single row given by the ID in the URI
+				selection = PetsEntry._ID + "=?";
+				selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+				return database.delete(PetsEntry.TABLE_NAME, selection, selectionArgs);
+			default:
+				throw new IllegalArgumentException("Deletion is not supported for " + uri);
+		}
 	}
 
 	/**

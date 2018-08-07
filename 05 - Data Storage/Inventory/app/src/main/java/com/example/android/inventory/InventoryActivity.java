6b501db35c1c0b811234665534fc7e0721272420
Teja Bayya <bayyatej.dev@gmail.com>
2018-08-06 20:33:35 -0400

    begin implementing LoaderCallbacks

--- 05 - Data Storage/Inventory/app/src/main/java/com/example/android/inventory/InventoryActivity.java
+++ 05 - Data Storage/Inventory/app/src/main/java/com/example/android/inventory/InventoryActivity.java
@@ -1,9 +1,17 @@
 package com.example.android.inventory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 
-public class InventoryActivity extends AppCompatActivity
+import com.example.android.inventory.data.InventoryContract.InventoryEntry;
+
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>
 {
 
 	@Override
@@ -11,5 +19,27 @@ public class InventoryActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inventory);
+	}
+
+	/*
+		Loader Callback Methods
+	 */
+	@NonNull
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args)
+	{
+		return new CursorLoader(this, InventoryEntry.CONTENT_URI, null, null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data)
+	{
+		//todo cursorAdapter.swap(data)
+	}
+
+	@Override
+	public void onLoaderReset(@NonNull Loader<Cursor> loader)
+	{
+		//todo cursorAdapter.swap(data)
 	}
 }

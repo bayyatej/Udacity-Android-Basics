e3fd544c573416bca8a95cd0c47cdee8337bd26f
Teja Bayya <bayyatej.dev@gmail.com>
2018-08-09 06:22:42 -0400

    add options menu to editor activity and fix menu overflow

--- 05 - Data Storage/Inventory/app/src/main/java/com/example/android/inventory/EditorActivity.java
+++ 05 - Data Storage/Inventory/app/src/main/java/com/example/android/inventory/EditorActivity.java
@@ -4,10 +4,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class EditorActivity extends AppCompatActivity
 {
 	private Uri currentUri;
+
+	private EditText mNameEditText,
+			mDescriptionEditText,
+			mPriceEditText,
+			mQuantityEditText;
+	private ImageButton mTakePictureButton,
+			mAddPhotoButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -32,6 +42,21 @@ public class EditorActivity extends AppCompatActivity
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		getMenuInflater().inflate(R.menu.menu_editor_activity, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.save_product:
+				//todo save product
+				break;
+			case R.id.delete_product:
+				//todo delete product
+				break;
+		}
 		return true;
 	}
 }
